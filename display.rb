1b6738b527eada0a76fb63886279aee02e027086
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-24 23:06:23 -0400

    Create Cursor and Display classes

---
+++ display.rb
@@ -0,0 +1,10 @@
+require "colorize"
+require_relative "board"
+require_relative "cursor"
+
+class Display
+    def initialize(board)
+        @board = board
+        @cursor = Cursor.new([0,0], board)
+    end
+end
