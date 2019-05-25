6fef7e0e93486bcd46bf096934f3ef66c418f92a
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-24 22:59:29 -0400

    Create Board#initialize, #move_piece; fill with dummy Pieces

---
+++ board.rb
@@ -0,0 +1,45 @@
+require_relative "piece"
+
+class Board
+    def initialize
+        @grid = Array.new(8) { [] }
+        @grid.each_with_index do |row, row_idx|
+            if [0,1,6,7].include?(row_idx)
+                8.times { row << Piece.new }
+            end
+        end
+    end
+
+    def move_piece(start_pos, end_pos)
+        piece = self[start_pos]
+        if piece.nil?
+            raise NoPieceError
+        elsif !piece.valid_move?(end_pos)
+            raise InvalidMoveError
+        else
+            self[start_pos], self[end_pos] = nil, piece
+        end
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, piece)
+        row, col = pos
+        @grid[row][col] = piece
+    end
+end
+
+class NoPieceError < StandardError
+    def message
+        "There is no piece to move from that start position."
+    end
+end
+
+class InvalidMoveError < StandardError
+    def message
+        "That piece cannot move to that end position."
+    end
+end
---
+++ piece.rb
@@ -0,0 +1,9 @@
+class Piece
+    def initialize
+
+    end
+
+    def valid_move?(end_pos)
+        true
+    end
+end
