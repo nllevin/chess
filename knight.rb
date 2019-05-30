36f651928007d703108910ddf71458d96fd98128
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-29 22:49:18 -0400

    Write Knight class

---
+++ knight.rb
@@ -0,0 +1,25 @@
+require_relative "piece"
+require_relative "stepable"
+
+class Knight < Piece
+    include Stepable
+
+    def symbol
+        :knight
+    end
+
+    protected
+
+    def move_diffs
+        [
+            [-2,1],
+            [-1,2],
+            [1,2],
+            [2,1],
+            [2,-1],
+            [1,-2],
+            [-1,-2],
+            [-2,-1]
+        ]
+    end
+end
