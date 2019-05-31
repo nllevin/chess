de124bcbc360f5fc4f23aa115b9bf44f80645604
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-31 18:46:31 -0400

    Write Piece#to_s

--- piece.rb
+++ piece.rb
@@ -1,11 +1,40 @@
 require_relative "board"
+require "colorize"
 
 class Piece
-    attr_reader :pos
+    attr_reader :pos, :color
 
     def initialize(color, board, pos)
         @color, @board, @pos =
             color, board, pos
+    end
+
+    def to_s
+        case symbol
+        when :rook
+            str = "R"
+        when :knight
+            str = "H"
+        when :bishop
+            str = "B"
+        when :king
+            str = "K"
+        when :queen
+            str = "Q"
+        when :pawn
+            str = "P"
+        when :null_piece
+            str = " "
+        end
+
+        case color
+        when :white
+            str = str.white
+        when :black
+            str = str.black
+        end
+
+        str
     end
 
     def moves
