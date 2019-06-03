a9319ec3adf4ffb5171a67068a344f27f5de5e1a
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-06-02 22:48:41 -0400

    Add option in Display to show debug info

--- display.rb
+++ display.rb
@@ -6,9 +6,9 @@ require_relative "cursor"
 class Display
     attr_reader :cursor, :board
     
-    def initialize(board)
-        @board = board
-        @cursor = Cursor.new([0,0], board)
+    def initialize(board, debug)
+        @board, @cursor, @debug = 
+            board, Cursor.new([0,0], board), debug
     end
 
     def render
@@ -32,11 +32,17 @@ class Display
             puts
         end
         puts "  #{("a".."h").to_a.join("")}"
+        
+        if @debug
+            puts "White in check: #{@board.in_check?(:white)}"
+            puts "Black in check: #{@board.in_check?(:black)}"
+            print @board[@cursor.cursor_pos].moves if @cursor.selected
+        end
     end
 end
 
 if __FILE__ == $PROGRAM_NAME
-    test_display = Display.new(Board.new)
+    test_display = Display.new(Board.new, true)
     while true
         test_display.render
         test_display.cursor.get_input
