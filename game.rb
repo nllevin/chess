fdab68eea70452e9cb885077fd22b9b484db24d6
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-06-03 15:15:46 -0400

    Start Game#play; write Game#swap_turn!

--- game.rb
+++ game.rb
@@ -1,10 +1,29 @@
 require_relative "display"
+require_relative "human_player"
 
 class Game
+    attr_reader :current_player
+
     def initialize
         @board = Board.new
         @display = Display.new(@board, false)
-        @players = { :white => HumanPlayer.new, :black => HumanPlayer.new }
+        @players = { 
+            :white => HumanPlayer.new(:white, @display), 
+            :black => HumanPlayer.new(:black, @display) 
+        }
         @current_player = :white 
+    end
+
+    def play
+        until @board.checkmate?(:white) || @board.checkmate?(:black)
+            @players[@current_player].make_move
+            swap_turn!
+        end
+    end
+
+    private
+
+    def swap_turn!
+        @current_player = @players.keys.find { |player| player != @current_player }
     end
 end
