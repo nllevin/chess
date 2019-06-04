a875cc62a344033d4463ef4dbe6963247646766c
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-06-03 21:59:53 -0400

    Add HumanPlayer#make_move

--- human_player.rb
+++ human_player.rb
@@ -4,4 +4,19 @@ class HumanPlayer
     def initialize(color, display)
         @color, @display = color, display
     end
+
+    def make_move(board)
+        cursor = @display.cursor
+        start_pos = nil
+        while start_pos.nil?
+            @display.render
+            start_pos = cursor.get_input
+        end
+        end_pos = nil
+        while end_pos.nil?
+            @display.render
+            end_pos = cursor.get_input
+        end
+        board.move_piece(start_pos, end_pos)
+    end
 end
